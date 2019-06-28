2956ea30e0cfef82dc2a6bd7533760733b660643
KevinPuusild <52325928+KevinPuusild@users.noreply.github.com>
2019-06-28 19:42:47 +0300

    Add files via upload

---
+++ VMware_DvPortgroup_Export_Text.ps1
@@ -0,0 +1,9 @@
+﻿#Võtan VDSwitchi Muutujasse
+$VDSwitch = "VDSwitchi_nimi"
+#Võtan asukoha kuhu tahan VLANi nimekirja exportida muutujasse
+$Export_to = "#ASUKOHT/vlan.txt"
+
+#Selekteerin kõik DvPortgroupid ja ekpordin nimekirja koos VLANidega välja
+Get-VirtualSwitch -Name $VDSwitch | Get-VirtualPortGroup | `
+
+Select Name, @{N="VLANId";E={$_.Extensiondata.Config.DefaultPortCOnfig.Vlan.VlanId}} > $Export_to
