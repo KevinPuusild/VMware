a1206b8cc23b550dc9c6fa0f76cf24a84c4f3d95
KevinPuusild <52325928+KevinPuusild@users.noreply.github.com>
2019-06-28 19:11:04 +0300

    Add files via upload

---
+++ VMware_Shutdown_Only_PowerOn_VMS.ps1
@@ -0,0 +1,7 @@
+﻿#Võtan Vmware "Clusteri" muutujasse
+$Cluster = "#Cluster"
+$myCluster = Get-DatastoreCluster -Name $Cluster
+#Otsin kõiki masinaid antud "Clusterist" mis töötavad
+$allVms = Get-VM -Datastore $myCluster | where { $_.PowerState -eq “PoweredOn”}
+#Panen need masinad kinni ning EI küsi üle et kas oled ikka nõus
+Stop-VM -VM $allVms -Confirm:$false
